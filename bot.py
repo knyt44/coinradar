4be21654da8d417a9b0cb04c8630d25675f251f7
knyt44 <kenany0644@gmail.com>
2026-03-25 02:56:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ import requests
 # ============================================================
 # CONFIG
 # ============================================================
-BINANCE_BASE_URL = "https://fapi.binance.de"
+BINANCE_BASE_URL = "https://fapi.binance.tr"
 STATE_FILE = "super_binance_spot_scanner_final_state.json"
 LOG_FILE = "super_binance_spot_scanner_final.log"
 HTTP_TIMEOUT = 15
